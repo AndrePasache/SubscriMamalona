73f6bedb179007bc32de8a295c83427eca9abeb5
adri-02 <adrianame298@gmail.com>
2020-05-28 00:03:56 -0500

    modimamalona285

--- app/src/main/java/com/application/subscrimamalona/Splash_Mn.java
+++ app/src/main/java/com/application/subscrimamalona/Splash_Mn.java
@@ -2,7 +2,9 @@ package com.application.subscrimamalona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Mn extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Splash_Mn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__mn);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Mn.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
