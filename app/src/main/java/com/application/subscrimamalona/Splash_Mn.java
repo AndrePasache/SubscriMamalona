da1f5addd28a083de04322e29bdcfb362df2642f
adri-02 <adrianame298@gmail.com>
2020-06-01 21:58:48 -0500

    Merge branch 'master' of C:\Users\Usuario\AndroidStudioProjects\SubscriMamalona with conflicts.

--- app/src/main/java/com/application/subscrimamalona/Splash_Mn.java
+++ app/src/main/java/com/application/subscrimamalona/Splash_Mn.java
@@ -16,7 +16,7 @@ public class Splash_Mn extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash_Mn.this, MainActivity.class);
+                Intent intent = new Intent(Splash_Mn.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
