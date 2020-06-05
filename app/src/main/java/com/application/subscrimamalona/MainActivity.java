386547336f4d9135a6fe40010fe40b4b5a81114d
xourge <xourge@gmail.com>
2020-06-05 16:21:23 -0500

    corregido: anadir

--- app/src/main/java/com/application/subscrimamalona/MainActivity.java
+++ app/src/main/java/com/application/subscrimamalona/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ;
+                Anadir(v);
             }
         });
 
